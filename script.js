afbf4c4a9f92be67c66efe2164aebf1c557f3075
isabelaandrade1 <isabela.andrade.hey@gmail.com>
2024-06-26 13:51:24 -0400

    functions adicionadas

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(input) {
+    const cleanedInput = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const length = cleanedInput.length;
+
+    for (let i = 0; i < length / 2; i++) {
+        if (cleanedInput[i] !== cleanedInput[length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+    if (arr.length === 0) {
+        return [null, null];
+    }
+
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return [min, max];
 }
+
